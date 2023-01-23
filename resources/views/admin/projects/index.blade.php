48290a9fdc5f9db501578111890dffbf548469f5
SaverioCascelli <saverio.cascelli@gmail.com>
2023-01-23 17:02:34 +0100

    [add] type in project show

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -5,26 +5,38 @@
         <div class="row">
             <div class="col">
 
-                <table class="table bg-white">
+                <h1>Project</h1>
+                <table class="table table-str iped">
                     <thead>
                         <tr>
-                            <th scope="col"><a href="{{ route('admin.orderby', ['id', $direction]) }}">ID</a></th>
-                            <th scope="col"><a href="{{ route('admin.orderby', ['name', $direction]) }}">Name</a></th>
-                            <th scope="col"><a href="{{ route('admin.orderby', ['client_name', $direction]) }}">Client
+                            <th class="table-id" scope="col"><a
+                                    href="{{ route('admin.orderby', ['id', $direction]) }}">ID</a></th>
+                            <th scope="col" class="table-name"><a
+                                    href="{{ route('admin.orderby', ['name', $direction]) }}">Name</a></th>
+                            <th scope="col" class="table-client-name"><a
+                                    href="{{ route('admin.orderby', ['client_name', $direction]) }}">Client
                                     Name</a></th>
-                            <th scope="col">actions</th>
+                            <th scope="col" class="table-type">Type </th>
+                            <th scope="col" class="table-actions">actions</th>
 
                         </tr>
                     </thead>
                     <tbody>
                         @foreach ($data as $project)
                             <tr>
-                                <th>{{ $project->id }}</th>
-                                <td>{{ $project->name }}</td>
-                                <td>{{ $project->client_name }}</td>
+                                <th scope="row sc-row">{{ $project->id }}</th>
+                                <td class="">{{ $project->name }}</td>
+                                <td class="">{{ $project->client_name }}</td>
                                 <td>
-                                    <a href="{{ route('admin.projects.show', $project) }}">show </a>
-                                    <a href="{{ route('admin.projects.edit', $project) }}">edit</a>
+                                    <h4><span class="badge bg-primary">{{ $project->type->name }}</span></h4>
+
+                                </td>
+                                <td class="">
+
+                                    <a class="btn btn-info " role="button"
+                                        href="{{ route('admin.projects.show', $project) }}">Show </a>
+                                    <a class="btn btn-warning "
+                                        role="button"href="{{ route('admin.projects.edit', $project) }}">Edit</a>
                                     @include('admin.partials.delete_button')
                                 </td>
                             </tr>
